d88f5e1b2e03793d8eb936102298691773eb65cf
awssteph <awssteph@amazon.com>
2021-10-29 14:49:08 +0100

    added csv greator

--- budgets.py
+++ budgets.py
@@ -1,11 +1,21 @@
 import boto3
 import os
+from lambda_base import generate_csv
 
 client = boto3.client('budgets')
 
 account_id = os.environ['account_id']
 paginator = client.get_paginator("describe_budgets") #Paginator for a large list of accounts
 response_iterator = paginator.paginate(AccountId=account_id)
+for budgets in response_iterator:
+    #import pdb; pdb.set_trace()
+    csv = generate_csv(budgets['Budgets'])
+file = open("budget.csv", "w")
+file.write(csv)
+file.close()
 
-for account in response_iterator:
-    print(account)
+# for budgets in response_iterator:
+#     for budget in budgets['Budgets']:
+#         print(budget)
+#         budget['BudgetName']
+#         budget['BudgetLimit']['Amount']
